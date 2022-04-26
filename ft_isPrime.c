c69a93dd679d27466014d2e2b94b421df860bb4f
wa5ina <ennasry.rida@gmail.com>
2022-04-26 18:10:27 +0000

    solving minor bugs

--- ft_isPrime.c
+++ ft_isPrime.c
@@ -6,19 +6,19 @@
 /*   By: ren-nasr <ren-nasr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 15:57:53 by ren-nasr          #+#    #+#             */
-/*   Updated: 2022/04/26 16:01:28 by ren-nasr         ###   ########.fr       */
+/*   Updated: 2022/04/26 18:09:26 by ren-nasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+// ft_isPrime.c is a function that checks if a number is prime or not. 
+// by checking if the number is divisible by any number between 2 and the number divided by 2 . */
 
 
-/*
-ft_isPrime.c is a function that checks if a number is prime or not. 
-by checking if the number is divisible by any number between 2 and the number divided by 2 . */
+#include "libft.h"
 
-int ft_isPrime(size_t ing n) {
-    size_t i;
-    
+long int    ft_isPrime(long int n) {
+    long int i;
+
     if (n <= 1) {
         return 0;
     }
