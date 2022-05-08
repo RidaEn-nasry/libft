07e93b30f9d61d88742b07713b3b39c16e94d104
wa5ina <ennasry.rida@gmail.com>
2022-05-08 16:02:35 +0100

    silly mistake

--- ft_safeFree.c
+++ ft_safeFree.c
@@ -6,19 +6,21 @@
 /*   By: ren-nasr <ren-nasr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 15:57:00 by ren-nasr          #+#    #+#             */
-/*   Updated: 2022/05/08 15:58:42 by ren-nasr         ###   ########.fr       */
+/*   Updated: 2022/05/08 16:02:25 by ren-nasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* 
     ft_safeFree:
         @description:
-            - free a pointer if it is not NULL
+            - free a pointer if it is not NULL 
         @param:
             - void *ptr: pointer to free
         @return:
             - void
 */
+
+#include "libft.h"
 
 void    ft_safeFree(void *ptr)
 {
