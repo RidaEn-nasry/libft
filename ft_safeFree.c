9a732d83ca51b3541ea02e1707b041e76929ce72
wa5ina <ennasry.rida@gmail.com>
2022-05-08 16:00:36 +0100

    minimal but efficient

---
+++ ft_safeFree.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_safeFree.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ren-nasr <ren-nasr@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/08 15:57:00 by ren-nasr          #+#    #+#             */
+/*   Updated: 2022/05/08 15:58:42 by ren-nasr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* 
+    ft_safeFree:
+        @description:
+            - free a pointer if it is not NULL
+        @param:
+            - void *ptr: pointer to free
+        @return:
+            - void
+*/
+
+void    ft_safeFree(void *ptr)
+{
+    if (ptr)
+        free(ptr);
+}
