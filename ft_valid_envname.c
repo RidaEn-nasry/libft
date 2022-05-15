ecbc5c5ae773841950f24b394d85df84723ed59b
wa5ina <rida.ennasry@gmail.com>
2022-05-15 13:07:51 +0100

    adding ft_valid_envname function

---
+++ ft_valid_envname.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_validenvname.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ren-nasr <ren-nasr@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/15 11:48:29 by ren-nasr          #+#    #+#             */
+/*   Updated: 2022/05/15 11:54:41 by ren-nasr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+    @description:
+        - check if the given string is a valid env variable name
+        - valid env variable name must start with a letter and can contain onlt letters, numbers and underscores
+    @param:
+        - char *env_name: the string to check
+    @return:
+        - bool: true if the string is a valid env variable name, false otherwise
+    @complexity:
+        - O(n)
+*/
+
+#include "libft.h"
+
+bool    ft_valid_envname(char *env_name)
+{
+    size_t  i;
+
+    if (!env_name)
+        return (false);
+    if (!ft_isalpha(env_name[0]))
+        return (false);
+    i = 0;
+    while (env_name[i])
+    {
+        if (!ft_isalnum(env_name[i]) && env_name[i] != '_')
+            return (false);
+        i++;
+    }
+    return (true);    
+}
