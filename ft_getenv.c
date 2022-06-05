c61d665fa2b64468ddcb76245a4f79f5233a002b
wa5ina <ren-nasr@student.1337.ma>
2022-06-05 12:57:50 +0100

    a little fucking bug

--- ft_getenv.c
+++ ft_getenv.c
@@ -6,32 +6,36 @@
 /*   By: ren-nasr <ren-nasr@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 14:59:18 by ren-nasr          #+#    #+#             */
-/*   Updated: 2022/06/05 12:45:21 by ren-nasr         ###   ########.fr       */
+/*   Updated: 2022/06/05 12:57:30 by ren-nasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-    @description:   
-        - this function returns the value of the environment variable
-        - if the variable is not found, it returns NULL
-    @param:
-        - char *name: the name of the variable
-    @return:
-        - char *: the value of the variable
+	@description:   
+		- this function returns the value of the environment variable
+		- if the variable is not found, it returns NULL
+	@param:
+		- char *name: the name of the variable
+	@return:
+		- char *: the value of the variable
 */
 
 #include "libft.h"
 
-char    *ft_getenv(char **env, char *name)
+char	*ft_getenv(char **env, char *name)
 {
-    size_t  i;
+	size_t	i;
+	char	*env_name;
+	size_t	len;
 
-    i = 0;
-    while (env[i])
-    {
-        if (ft_strncmp(env[i], name, ft_strlen(env[i])) == 0)
-            return (env[i] + ft_strlen(name) + 1);
-        i++;
-    }
-    return (NULL);
+	len = ft_strlen(name);
+	i = 0;
+	while (env[i])
+	{
+		env_name = ft_substr(env[i], 0, ft_strchr(env[i], '=') - env[i]);
+		if (ft_strncmp(env_name, name, len) == 0)
+			return (env[i] + len + 1);
+		i++;
+	}
+	return (NULL);
 }
