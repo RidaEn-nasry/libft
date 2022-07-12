90bc52b4b2fb4c2062b919584cb3eb06b13480d4
RidaEn-nasry <65143740+RidaEn-nasry@users.noreply.github.com>
2022-07-12 18:08:37 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -10,13 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_BONUS_H
-# define GET_NEXT_LINE_BONUS_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
 
-char	*get_next_line_bonus(int fd);
+char	*get_next_line(int fd);
 
 #endif
