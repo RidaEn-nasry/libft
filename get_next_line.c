e23df471472809779bdf1a77ef3df1bbe392a330
RidaEn-nasry <65143740+RidaEn-nasry@users.noreply.github.com>
2022-07-12 18:09:00 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 static int	index_line(const char *s, int c)
 {
