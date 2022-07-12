0fb88ae221bb0d90754616d38fbe5e685b80b3eb
RidaEn-nasry <65143740+RidaEn-nasry@users.noreply.github.com>
2022-07-12 18:08:17 +0100

    Update get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -18,10 +18,5 @@
 # include <limits.h>
 
 char	*get_next_line_bonus(int fd);
-size_t	ft_strlen(const char *str);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strchr(const char *s, int c);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *s1);
 
 #endif
