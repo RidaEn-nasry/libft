1c4cea2fc7d872787b5c342cb43aa4c82987c28b
wa5ina <ren-nasr@student.1337.ma>
2022-07-13 15:32:02 +0100

    making exit actually exit

--- exitif.c
+++ exitif.c
@@ -1,28 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   exitIF.c                                           :+:      :+:    :+:   */
+/*   exitif.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ren-nasr <ren-nasr@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: ren-nasr <ren-nasr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/21 21:55:54 by ren-nasr          #+#    #+#             */
-/*   Updated: 2022/04/21 21:58:25 by ren-nasr         ###   ########.fr       */
+/*   Updated: 2022/07/13 15:31:46 by ren-nasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* exitIF.c implementation :
+/* exitif.c implementation :
 	- exit with error message if condition is true	
 	- return 0 if condition is false
 */
 
 #include "libft.h"
 
-int	exitIF(int condition, char *message)
+int	exitif(int condition, char *message)
 {
 	if (condition)
 	{
 		ft_putendl_fd(message, 2);
-		return(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	return (0);
 }
